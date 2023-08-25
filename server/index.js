05d9ad32281165318da3efb155d4add0a1df98db
Sergi <sergikusakin@gmail.com>
2023-08-25 14:25:35 +0200

    crear async fn

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 
+// const sequelize = require("/db.js");
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.listen(PORT, () => console.log(`Server has been started on ${PORT}`));
+const start = async () => {
+  try {
+    app.listen(PORT, () => console.log(`Server has been started on ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
