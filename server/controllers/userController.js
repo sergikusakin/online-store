12b2ccfd56388b8a08decb486adb8feaedce03e7
Sergi <sergikusakin@gmail.com>
2023-09-07 15:05:36 +0200

    middleware

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,17 @@
+const apiError = require("../error/apiError");
+
 class UserController {
   async registration(req, res) {}
 
   async login(req, res) {}
 
-  async check(req, res) {}
+  async check(req, res, next) {
+    const { id } = req.query;
+    if (!id) {
+      return next(apiError.badRequest(" ID is not found "));
+    }
+    res.json(id);
+  }
 }
 
 module.exports = new UserController();
--- server/index.js
+++ server/index.js
@@ -1,21 +1,19 @@
 require("dotenv").config();
 const express = require("express");
-
 const sequelize = require("./db.js");
-
 const models = require("./models/models.js");
-
 const cors = require("cors");
-
 const router = require("./routes/index.js");
+const errorHandler = require("./middleware/errorHandlingMinddleware.js");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 app.use("/api", router);
+//error handling, middleware last
+app.use(errorHandler);
 
 const start = async () => {
   try {
