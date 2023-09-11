ddeee693591b834f023fd12269ed5de08d9fed52
Sergi <sergikusakin@gmail.com>
2023-09-11 17:22:50 +0200

    i express-fileupload

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const sequelize = require("./db.js");
 const models = require("./models/models.js");
 const cors = require("cors");
+const fileUpload = require("express-fileupload");
 const router = require("./routes/index.js");
 const errorHandler = require("./middleware/errorHandlingMinddleware.js");
 
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload({}));
 app.use("/api", router);
 //error handling, middleware last
 app.use(errorHandler);
