e7d4332cfea232b3d2a53d9b633b8a403768ef79
Sergi <sergikusakin@gmail.com>
2023-09-12 14:41:06 +0200

    getting devices and filtration

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,12 +1,12 @@
 const uuid = require("uuid");
 const path = require("path");
-const { Device } = require("../models/models.js");
+const { Device, DeviceInfo } = require("../models/models.js");
 const apiError = require("../error/apiError.js");
 
 class DeviceController {
   async create(req, res, next) {
     try {
-      const { name, price, brandId, typeId, info } = req.body;
+      let { name, price, brandId, typeId, info } = req.body;
       const { img } = req.files;
       let fileName = uuid.v4() + ".jpg";
       img.mv(path.resolve(__dirname, "..", "static", fileName));
@@ -19,12 +19,58 @@ class DeviceController {
         img: fileName,
       });
 
+      if (info) {
+        info = JSON.parse(info);
+        info.forEach((i) =>
+          DeviceInfo.create({
+            title: i.title,
+            description: i.description,
+            deviceId: device.id,
+          })
+        );
+      }
+
       return res.json(device);
     } catch (e) {
       next(apiError.badRequest(e.message));
     }
   }
-  async getAll(req, res) {}
+  async getAll(req, res) {
+    let { brandId, typeId, limit, page } = req.query;
+    page = page || 1;
+    limit = limit || 9;
+    let offset = page * limit - limit;
+
+    let devices;
+
+    if (!brandId && !typeId) {
+      devices = await Device.findAndCountAll({ limit, offset });
+    }
+
+    if (brandId && !typeId) {
+      devices = await Device.findAndCountAll({
+        where: { brandId },
+        limit,
+        offset,
+      });
+    }
+
+    if (!brandId && typeId) {
+      devices = await Device.findAndCountAll({
+        where: { typeId },
+        limit,
+        offset,
+      });
+    }
+
+    if (brandId && typeId) {
+      devices = await Device.findAndCountAll({
+        where: { brandId, typeId },
+      });
+    }
+
+    return res.json(devices);
+  }
 
   async getOne(req, res) {}
 }
