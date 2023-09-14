f7ff0325ea8bc5f7a8b913627c4dce4cc1ef4821
Sergi <sergikusakin@gmail.com>
2023-09-14 14:44:47 +0200

    registration, authorization, jwt token, bcrypt

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,35 @@
 const apiError = require("../error/apiError");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { User, Basket } = require("../models/models.js");
+
+const generateJwt = (id, email, role) => {
+  return jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
+    expiresIn: "24h",
+  });
+};
 
 class UserController {
-  async registration(req, res) {}
+  async registration(req, res, next) {
+    const { email, password, role } = req.body;
+
+    if (!email || !password) {
+      return next(apiError.badRequest("Email or password is incorrect"));
+    }
+
+    const candidate = await User.findOne({ where: { email } });
+
+    if (candidate) {
+      return next(apiError.badRequest("User with this email already exists"));
+    }
+
+    const hashPassword = await bcrypt.hash(password, 5);
+    const user = await User.create({ email, role, password: hashPassword });
+    const basket = await Basket.create({ userId: user.id });
+    const token = generateJwt(user.id, user.email, user.role);
+
+    return res.json({ token });
+  }
 
   async login(req, res) {}
 
